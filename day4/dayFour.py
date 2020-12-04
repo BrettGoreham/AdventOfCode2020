4943cf79a8ff18338c981ed29abe473b1d5bae15
Brett Goreham <brettgoreham@gmail.com>
2020-12-04 12:54:55 +0100

    clean up on partials that could just be lambdas

--- day4/dayFour.py
+++ day4/dayFour.py
@@ -19,27 +19,13 @@ def check_not_valid_height(val):
     return True
 
 
-def check_hair_color(val): return val[0] != '#' or len(val) != 7
-
-
-def check_eye_color(val): return val not in validEyeColors
-
-
-def check_person_id(val): return not val.isdigit() or len(val) != 9
-
-
-byrCheck = partial(check_not_valid_int, 1920, 2002)
-iyrCheck = partial(check_not_valid_int, 2010, 2020)
-eyrCheck = partial(check_not_valid_int, 2020, 2030)
-hgtCheck = partial(check_not_valid_height)
-hclCheck = partial(check_hair_color)
-eclCheck = partial(check_eye_color)
-pidCheck = partial(check_person_id)
-
-requiredFieldsAndCheckLambda = (('byr', byrCheck), ('iyr', iyrCheck),
-                                ('eyr', eyrCheck), ('hgt', hgtCheck),
-                                ('hcl', hclCheck), ('ecl', eclCheck),
-                                ('pid', pidCheck))
+requiredFieldsAndCheckLambda = (('byr', partial(check_not_valid_int, 1920, 2002)),
+                                ('iyr', partial(check_not_valid_int, 2010, 2020)),
+                                ('eyr', partial(check_not_valid_int, 2020, 2030)),
+                                ('hgt', partial(check_not_valid_height)),
+                                ('hcl', lambda a: a[0] != '#' or len(a) != 7),
+                                ('ecl', lambda a: a not in validEyeColors),
+                                ('pid', lambda a: not a.isdigit() or len(a) != 9))
 
 
 def check_passport_field_validity(field_to_value_dict):
